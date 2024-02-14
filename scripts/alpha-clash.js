f9283edfb830e13a9bdc71d63c737807ba7a0fa6
Ahmed Noorim <ahmed.noorim13@gmail.com>
2024-02-15 03:05:31 +0600

    keyup event handled

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,3 +1,22 @@
+function handleKeyboardKeyUpEvent(event) {
+    const keyPressed = event.key
+    const targetAlpha = document.getElementById('current-alpha').innerText.toLowerCase()
+    console.log('Pressed:', keyPressed, 'Target:', targetAlpha)
+
+    // check match
+    if (keyPressed === targetAlpha) {
+        console.log('you get a point!')
+        removeBackColorById(targetAlpha)
+        continueGame()
+    }
+    else {
+        console.log('you lost a life!')
+    }
+
+}
+// capture keypress
+document.addEventListener('keyup', handleKeyboardKeyUpEvent)
+
 function continueGame() {
     const alphabet = getRandomAlphabet()
     console.log("Random Alphabet:", alphabet)
